84c697bd8e6def419489e421077ca896bf396469
lucastanh <lucasvinicius564@hotmail.com>
2023-04-04 20:04:25 -0300

    refactor: decoupling websocket instance

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,15 @@
 import express, { type Application, json } from 'express'
 import routes from './routes/routes'
 import cors from 'cors'
-import Wss from './websocket'
-import { type Server, createServer } from 'http'
 
 class App {
   express: Application
-  wss: Wss
-  server: Server
 
   constructor() {
     this.express = express()
 
     this._routes()
     this._middlewares()
-    this._webSocket()
   }
 
   _routes(): void {
@@ -25,11 +20,6 @@ class App {
     this.express.use(json())
     this.express.use(cors())
   }
-
-  _webSocket(): void {
-    this.server = createServer(this.express)
-    this.wss = new Wss(this.server)
-  }
 }
 
-export default new App()
+export default new App().express
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import app from './app'
+import Wss from './websocket'
 
 const server = (): void => {
   const PORT = 3000
 
   try {
-    app.server.listen(PORT, () => {
+    const server = app.listen(PORT, () => {
       console.log(`Server is running at http://localhost:${PORT}`)
     })
+
+    Wss.init(server)
   } catch (error) {
     console.log('Server starting ERROR: ', error)
     process.exit(1)
